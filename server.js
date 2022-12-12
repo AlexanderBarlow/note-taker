6b745fd62dac3444fc8b06a8dd1d29b670439077
alex barlow <alex.barlow@rocketmail.com>
2022-12-12 17:28:18 -0500

    added methodology from class

--- server.js
+++ server.js
@@ -2,22 +2,26 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const notes = require('./db/db.json');
+const uuid = require('./public/assets/js/uuid')
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
+
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
+
 app.get('/api/notes', (req, res) => {
-    res.json(notes.slice(1))
+    res.json(notes);
 });
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 });
+
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
@@ -27,29 +31,81 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
 });
 
-const createNote = (body, createdNotes) => {
-    const note = body;
-    if (!Array.isArray(createdNotes))
-        createdNotes = [];
-    
-    if (createdNotes.length === 0)
-        createdNotes.push(0);
 
-    body.id = createdNotes[0];
-    createdNotes[0]++;
+//Above code comes from directions within GitLab
+//TODO: Code from class examples
+app.post('/api/notes', (req,res) => {
 
-    createdNotes.push(note);
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify(createdNotes, null, 2)
-    );
-    return note;
-}
+const { title, note } = req.body;
 
-app.post('/api/notes', (req, res) => {
-    const note = createNote(req.body, notes);
-    res.json(note);
+if(title && note) {
+    const newNote = {
+        title,
+        note,
+        id: uuid(),
+    };
+    console.log(newNote);
+
+    const newNotes = (newNote);
+
+    fs.readFile(`./db/db.json`, ('utf8'), (err,data) => {
+        if(err) {
+            console.log(err);
+        }else {
+            const note = JSON.parse(data);
+
+            note.push(newNotes);
+
+            fs.writeFile(`./db/db.json`, JSON.stringify(note, null, 4), (writeErr) => 
+                writeErr 
+                ? console.log(writeErr)
+                : console.log(`New note has been made.`)
+            );
+        };
+    });
+   
+    const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      console.log(response);
+      res.status(201).json(response);
+}else {
+    res.status(500).json('Error in saving note')
+};
 });
+//TODO: Code from class examples
+
+
+
+// TODO: THE CODE BELOW HAS THE APPLICATION FUNCTIONING
+// const createNote = (body, createdNotes) => {
+
+//     const note = body;
+
+//     if (!Array.isArray(createdNotes))
+//         createdNotes = [];
+    
+//     if (createdNotes.length === 0)
+//         createdNotes.push(0);
+
+//     body.id = createdNotes[0];
+//     createdNotes[0]++;
+
+//     createdNotes.push(note);
+//     fs.writeFileSync(
+//         path.join(__dirname, './db/db.json'),
+//         JSON.stringify(createdNotes, null, 2)
+//     );
+//     return note;
+// }
+
+// app.post('/api/notes', (req, res) => {
+//     const note = createNote(req.body, notes);
+//     res.json(note);
+// });
+//TODO: END OF FUNCTION CODE
 
 app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT}`);
