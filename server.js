a1670b47f282acacedfa48974b36e67906d999e0
alex barlow <alex.barlow@rocketmail.com>
2022-12-12 14:58:29 -0500

    required notes.html and added path /api/notes

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const notes = require('./db/db.json');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
@@ -14,6 +18,19 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
+
+app.get('api/notes', (req, res) => {
+    res.json(notes.slice(1))
+});
+
+const createNote = (body, createdNotes) => {
+    const note = body;
+    
+}
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/index.html'))
+// });
 
 app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT}`);
